de6092412f89301a9c0f4cbfdc48e918094d8c12
Song Shik <song.shik@sa365.ag>
2024-11-01 15:10:05 -0300

    feat: alterações de finais

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # awsLambda
+## Codigo de teste da AWS Lambda
+Implementação de uma função Lambda chamada handler que verifica o tamanho quando um objeto é armazenado em uma S3. 
+Para isso também foi implementado CI/CD com GitHub Actions para automação de deploy.
--- index.mjs
+++ index.mjs
@@ -1,15 +1,27 @@
 import { log } from "./log.mjs";
+import { S3 } from '@aws-sdk/client-s3';
+
+
+const s3Client = new S3({ region: 'us-east-1' });
 
 export const handler = async (event) => {
 
-    log("CI/CD : ", JSON.stringify(event));
+    const record = event.Records[0];
+    const Bucket = record.s3.bucket.name;
+    const key = record.s3.object.key;
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify(event),
-        body: `<html><body>Dados da requisicao ${JSON.stringify(event)}</body></html>`,
-        headers: {
-            "content-type": "text/html"
-        }
-    };
+    const getObjectResult = await s3Client.getObject({
+        Bucket,
+        Key,
+    });
+    const mega_byte = 1024 * 1024;
+
+    if (getObjectResult.ContentLength > 1 * mega_byte) {
+        log('Objeto muito grande');
+
+        return 'Objeto muito grande';
+    }
+
+    log('Objeto de tamanho OK');
+    return 'Objeto de tamanho OK';
 };
