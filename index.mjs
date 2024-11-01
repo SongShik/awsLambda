54dcfaedef522963b0f298e7fa92f25c8f6da3ba
Song Shik <song.shik@sa365.ag>
2024-11-01 12:05:07 -0300

    feat: tratando resposta da função

--- index.mjs
+++ index.mjs
@@ -1,12 +1,15 @@
 import { log } from "./log.mjs";
 
 export const handler = async (event) => {
-    
+
     log("CI/CD : ", JSON.stringify(event));
 
     return {
         statusCode: 200,
         body: JSON.stringify(event),
-      };;
-  };
-  
+        body: `<html><body>Dados da requisicao ${JSON.stringify(event)}</body></html>`,
+        headers: {
+            "content-type": "text/html"
+        }
+    };
+};
